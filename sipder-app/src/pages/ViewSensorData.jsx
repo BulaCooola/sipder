0d12697a6b3f29368812c565fec7f831a13d5619
bbulatao <bbulatao@stevens.edu>
2025-05-05 12:49:40 -0400

    page changes

---
+++ sipder-app/src/pages/ViewSensorData.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default ViewSensorData= () => {
+    return (
+        <>
+            <div>
+                Recorded Sensor Data
+
+                DELETE LATER: TODO
+                In order to retrieve the data from the arm
+                Backend
+                - Listen and receive the data
+                - once server receives the data, save it to a file name it WITH AN INDEX (i.e, recording1)
+                - save the name of the file, into the file within the sipder app
+
+                Frontend
+                - create a div to put the list of filenames; it should display a visualization of data
+                - create a page to display the graphs
+            </div>
+        </>
+    )
+}
